657cdab5a24f31cee80aa896464cd0d17cb70f30
Matheus <matheus.nobrega.duarte@gmail.com>
2023-07-14 02:12:35 -0300

    Map model done

--- src/main/java/com/example/springboot/models/MapModel.java
+++ src/main/java/com/example/springboot/models/MapModel.java
@@ -18,4 +18,44 @@ public class MapModel implements Serializable {
     private String country;
     private Date dateAdded;
     private boolean activePool;
+
+    public UUID getIdMap() {
+        return idMap;
+    }
+
+    public void setIdMap(UUID idMap) {
+        this.idMap = idMap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public boolean isActivePool() {
+        return activePool;
+    }
+
+    public void setActivePool(boolean activePool) {
+        this.activePool = activePool;
+    }
 }
